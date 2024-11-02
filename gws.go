c063155ced11f503c1aa24ef89d0030e483f274f
Ahmad Yousuf <ahmadoyousuf@lewisu.edu>
2024-11-02 18:28:34 -0500

    update

--- gws.go
+++ gws.go
@@ -3,11 +3,19 @@ package main
 import (
     "fmt"
     "log"
+    "math/rand"
     "net/http"
+    "time"
     "gws/hello"
     "gws/syllabus"
     "gws/help"
 )
+
+func d6Handler(w http.ResponseWriter, r *http.Request) {
+    rand.Seed(time.Now().UnixNano())
+    roll := rand.Intn(6) + 1
+    fmt.Fprintf(w, "You rolled a: %d", roll)
+}
 
 func main() {
     http.HandleFunc("/hello-world", hello.HelloWorld)
@@ -15,6 +23,7 @@ func main() {
     http.HandleFunc("/hello-world-json", hello.HelloWorldJSON)
     http.HandleFunc("/syllabus", syllabus.SyllabusHandler)
     http.HandleFunc("/help", help.HelpHandler)
+    http.HandleFunc("/d6", d6Handler)
 
     fmt.Println("Starting server on :8080...")
     if err := http.ListenAndServe(":8080", nil); err != nil {
